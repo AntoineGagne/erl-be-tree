ccf1fdf3536f1607b176adca668900aafa69be4b
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2022-05-27 13:54:34 -0400

    Use the new `-nifs` compiler attribute

--- src/erl_betree_nif.erl
+++ src/erl_betree_nif.erl
@@ -1,6 +1,7 @@
 -module(erl_betree_nif).
 
 -compile(no_native).
+
 -on_load(on_load/0).
 
 -export([
@@ -11,35 +12,47 @@
     betree_search/2
 ]).
 
--spec on_load() -> ok.
+-nifs([
+    betree_make/1,
+    betree_make_sub/4,
+    betree_insert_sub/2,
+    betree_exists/2,
+    betree_search/2
+]).
 
+-spec on_load() -> ok.
 on_load() ->
-    SoName = case code:priv_dir(erl_betree) of
-        {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, erl_betree]);
-                _ ->
-                    filename:join([priv, erl_betree])
-            end;
-        Dir ->
-            filename:join(Dir, erl_betree)
-    end,
+    SoName =
+        case code:priv_dir(erl_betree) of
+            {error, bad_name} ->
+                case filelib:is_dir(filename:join(["..", priv])) of
+                    true ->
+                        filename:join(["..", priv, erl_betree]);
+                    _ ->
+                        filename:join([priv, erl_betree])
+                end;
+            Dir ->
+                filename:join(Dir, erl_betree)
+        end,
     ok = erlang:load_nif(SoName, 0).
 
 %% shamelessly stolen from crypto.erl
--define(nif_stub,nif_stub_error(?LINE)).
+-define(nif_stub, nif_stub_error(?LINE)).
+
 nif_stub_error(Line) ->
-    erlang:nif_error({nif_not_loaded,module,?MODULE,line,Line}).
+    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, Line}).
 
 betree_make(_Domains) ->
     ?nif_stub.
+
 betree_make_sub(_Betree, _SubId, _Constants, _Expr) ->
     ?nif_stub.
+
 betree_insert_sub(_Betree, _Sub) ->
     ?nif_stub.
+
 betree_exists(_Betree, _Event) ->
     ?nif_stub.
+
 betree_search(_Betree, _Event) ->
     ?nif_stub.
-
